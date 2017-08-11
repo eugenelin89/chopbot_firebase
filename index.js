ed9a2a6a3ad9bc6b88f2e4fec1d95145db271598
Eugene Lin <eugenelin@Eugenes-MacBook-Pro.local>
2017-08-11 13:46:03 -0700

    test

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ console.log(process.env.FIREBASE_PRIVATE_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert({
-    "private_key": process.env.FIREBASE_PRIVATE_KEY,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     "client_email": process.env.FIREBASE_CLIENT_EMAIL,
   }),
   databaseURL: "https://chopbase.firebaseio.com"
