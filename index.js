8dba63c3e2d011d89793a46dea10c0a088d6b3d4
Eugene Lin <eugenelin@Eugenes-MacBook-Pro.local>
2017-08-11 15:39:32 -0700

    test

--- index.js
+++ index.js
@@ -26,7 +26,11 @@ router.use(function(req, res, next) {
     if(process.env.CHOPBASE_TOKEN != req.get('CHOPBASE_TOKEN')){
         console.log('Bad Token')
         //res.status(401).send('Unauthorized')
-        throw new Error();
+        try {
+            throw new Error('Unauthorized');
+        } catch(e) {
+            next(e);
+        }
     }else{
         console.log('Good Token')
     }
