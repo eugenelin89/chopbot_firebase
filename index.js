32eb8ffbabe459d113424ce324d3ac4624e42b37
Eugene Lin <eugenelin@Eugenes-MacBook-Pro.local>
2017-08-14 11:14:06 -0700

    test

--- index.js
+++ index.js
@@ -92,8 +92,8 @@ app.use('/v1', router);
 app.use(function(err, req, res, next){
     console.log('An error has occurred... ')
     //console.error(err.stack);
-    //res.send(500, 'FAIL...');
-    res.status(err.status || 403);
+    res.send(500, 'FAIL...');
+    //res.status(err.status || 403);
 });
 
 app.listen(app.get('port'), function() {
